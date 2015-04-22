223a358f7eefc33d661546a5d4fefb1364399d09
Jorge Pardiñas <jparbros@gmail.com>
2015-04-22 09:50:20 -0500

    fixing logout

--- app/controllers/devise_token_auth/sessions_controller.rb
+++ app/controllers/devise_token_auth/sessions_controller.rb
@@ -56,6 +56,27 @@ module DeviseTokenAuth
       end
     end
 
+    def destroy
+      # remove auth instance variables so that after_filter does not run
+      user = remove_instance_variable(:@resource) if @resource
+      client_id = remove_instance_variable(:@client_id) if @client_id
+      remove_instance_variable(:@token) if @token
+
+      if user and client_id and user.tokens[client_id]
+        user.tokens.delete(client_id)
+        user.save!
+
+        render json: {
+          success:true
+        }, status: 200
+
+      else
+        render json: {
+          errors: ["User was not found or was not logged in."]
+        }, status: 404
+      end
+    end
+
     def valid_params?
       resource_params[:password] && resource_params[:email]
     end
