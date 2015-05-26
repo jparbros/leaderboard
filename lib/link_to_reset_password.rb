b3bb3724324764c514f3102e2ffa7a75e672ca35
Jorge Pardiñas <jparbros@gmail.com>
2015-05-26 07:27:42 -0500

    Fixing templates

--- lib/link_to_reset_password.rb
+++ lib/link_to_reset_password.rb
@@ -2,6 +2,7 @@ class LinkToResetPassword < Liquid::Tag
   include ActionView::Helpers::UrlHelper
 
   def initialize(tag_name, params, tokens)
+    @routes = Rails.application.routes.url_helpers
     @params = params
     super
   end
@@ -9,7 +10,7 @@ class LinkToResetPassword < Liquid::Tag
   def render(context)
     vars = context.environments.first
     host = Rails.application.config.action_mailer[:default_url_options][:host]
-    link_to @params, "http://#{vars[:subdomain]}.#{host}/auth/password/edit?redirect_url=http://#{vars[:subdomain]}.#{host}/profile&reset_password_token=#{vars[:token]}"
+    link_to @params, @routes.edit_user_password_url(reset_password_token: vars['token'], subdomain: vars['subdomain'], redirect_url: @routes.profile_url(subdomain: vars['subdomain']))
   end
 
 end
--- lib/link_to_signin.rb
+++ lib/link_to_signin.rb
@@ -8,9 +8,8 @@ class LinkToSignin < Liquid::Tag
 
   def render(context)
     vars = context.environments.first
-    profile_url = Rails.application.routes.url_helpers.profile_url(subdomain: vars[:subdomain])
     host = Rails.application.config.action_mailer[:default_url_options][:host]
-    link_to @params, "http://#{vars[:subdomain]}.#{host}/signin"
+    link_to @params, "http://#{vars['subdomain']}.#{host}/signin"
   end
 
 end
