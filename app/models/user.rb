af15eef89cebd58457ee9887f62383649117c0eb
Jorge Pardiñas <jparbros@gmail.com>
2015-08-04 09:23:33 -0500

    fix error

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
       reject_if: :all_blank
 
   validates :email, uniqueness: true
-  validates :name, presence: true
+  validates :name, presence: true, on: :update
 
   def confirmed?
    true
