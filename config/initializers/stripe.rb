51f199821f020b0cc0575c4e225145de10e6f4dc
Jorge Pardinas <jparbros@gmail.com>
2016-01-06 10:15:00 -0600

    Fixing error in stripe events

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,19 +3,19 @@ Stripe.api_key = "sk_test_mZww9eF34Na4z1kZz2E6Y9Of"
 StripeEvent.subscribe 'charge.succeeded' do |event|
   params = event.data.object
   puts "PARAMS -> #{params}"
-  organization = Organization.find_by_subscription_id(params['data']['object']['customer']).first
-  organization.create_transaction(params['data']['object']) if organization
+  organization = Organization.find_by_subscription_id(params).first
+  organization.create_transaction(params) if organization
 end
 
 StripeEvent.subscribe 'charge.failed' do |event|
   params = event.data.object
-  organization = Organization.find_by_subscription_id(params['data']['object']['customer']).first
-  organization.create_transactions(params['data']['object']) if organization
+  organization = Organization.find_by_subscription_id(params['customer']).first
+  organization.create_transactions(params) if organization
 end
 
 StripeEvent.subscribe 'customer.subscription.updated' do |event|
   params = event.data.object
   puts "PARAMS -> #{params}"
-  organization = Organization.find_by_subscription_id(params['data']['object']['customer']).first
-  organization.recurring_subscription(params['data']['object']) if organization
+  organization = Organization.find_by_subscription_id(params['customer']).first
+  organization.recurring_subscription(params) if organization
 end
