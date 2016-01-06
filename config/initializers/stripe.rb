eb7e2290ac1960a4464d70dd3c5ef1984dc295fd
Jorge Pardinas <jparbros@gmail.com>
2016-01-06 10:20:54 -0600

    Fixing error

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,18 +4,18 @@ StripeEvent.subscribe 'charge.succeeded' do |event|
   params = event.data.object
   puts "PARAMS -> #{params}"
   organization = Organization.find_by_subscription_id(params).first
-  organization.create_transaction(params) if organization
+  organization.subscription.create_transaction(params) if organization
 end
 
 StripeEvent.subscribe 'charge.failed' do |event|
   params = event.data.object
   organization = Organization.find_by_subscription_id(params['customer']).first
-  organization.create_transactions(params) if organization
+  organization.subscription.create_transactions(params) if organization
 end
 
 StripeEvent.subscribe 'customer.subscription.updated' do |event|
   params = event.data.object
   puts "PARAMS -> #{params}"
   organization = Organization.find_by_subscription_id(params['customer']).first
-  organization.recurring_subscription(params) if organization
+  organization.subscription.recurring_subscription(params) if organization
 end
