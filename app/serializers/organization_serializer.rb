c5d435c44a7dcc0fd9483cc3bbeb24b4ab2b6ae1
Jorge Pardinas <jparbros@gmail.com>
2016-02-09 10:07:20 -0600

    Adding data to billing page

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,4 +1,4 @@
 class OrganizationSerializer < ActiveModel::Serializer
-  attributes :id, :active, :days_left, :boardname, :rolling, :rolling_time, :name, :boardname, :subscribed, :vat, :company_name
+  attributes :id, :active, :days_left, :boardname, :rolling, :rolling_time, :name, :boardname, :subscribed, :vat, :company_name, :billing_email
   has_one :address
 end
