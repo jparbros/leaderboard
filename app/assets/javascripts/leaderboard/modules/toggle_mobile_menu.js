5335b7a46621a8fdc464ec303ef2efbcf1746199
Jorge Pardinas <jparbros@gmail.com>
2016-03-30 07:55:30 -0600

    Mobile changes

--- app/assets/javascripts/leaderboard/modules/toggle_mobile_menu.js
+++ app/assets/javascripts/leaderboard/modules/toggle_mobile_menu.js
@@ -8,3 +8,14 @@ LeaderboardApp.directive('toggleMobileMenu', function() {
     }
   };
 });
+
+LeaderboardApp.directive('toggleProfileMenu', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, elem, attrs) {
+      elem.bind('click', function() {
+         $('.main-row').toggleClass('profile-toggled');
+      });
+    }
+  };
+});
