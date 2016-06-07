c63756a38d4935b6838d30700caa7f21d6cdbb20
Jorge Pardinas <jparbros@gmail.com>
2016-06-07 09:35:11 -0500

    websockets config

--- config/initializers/websocket_rails.rb
+++ config/initializers/websocket_rails.rb
@@ -15,12 +15,12 @@ WebsocketRails.setup do |config|
   # Change to true to enable standalone server mode
   # Start the standalone server with rake websocket_rails:start_server
   # * Requires Redis
-  config.standalone = true
+  config.standalone = false
 
   # Change to true to enable channel synchronization between
   # multiple server instances.
   # * Requires Redis.
-  config.synchronize = true
+  config.synchronize = false
 
   # Prevent Thin from daemonizing (default is true)
   # config.daemonize = false
